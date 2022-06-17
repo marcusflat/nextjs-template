a634100545fd5b890d8738992c83a39b1eaf996e
marcusflat <marcus_fonseca@ymail.com>
2022-06-17 15:52:23 -0300

    feat: add styled components

--- src/components/templates/base/BaseTemplate.styles.tsx
+++ src/components/templates/base/BaseTemplate.styles.tsx
@@ -1,3 +1,8 @@
-export const Container: React.FC = ({ children }) => (
-  <div className="w-full h-full flex flex-col">{children}</div>
-);
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
